9da6af4cd7d1557fc1dc26cf89ecbf57cd37b530
ekrmabd <ekramhossainekram28@gmail.com>
2026-08-12 15:12:28 +0600

    method uda

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('login', [ApiController::class, 'login']);
 //
-Route::post('my-tickets', [ApiController::class, 'myTickets']);
+Route::get('my-tickets', [ApiController::class, 'myTickets']);
 
 Route::middleware('auth:api')->group(function () {
 	Route::post('update-device-token', [ApiController::class, 'updateDeviceToken']);
